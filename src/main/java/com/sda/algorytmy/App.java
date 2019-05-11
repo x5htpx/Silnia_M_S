8974e8c75af00f9ff18ff0fc9211cbc873a3e4a1
Michal Stusik <x5htpx@gmail.com>
2019-05-11 15:40:26 +0100

    rekurencja

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,51 @@
 package com.sda.algorytmy;
 
+import java.sql.SQLOutput;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+//zad2 11/5
+    public static double ciag(int n) {
+        if (n == 1) {
+            return 1;
+
+        }
+        if (n == 2) {
+            return 0.5;
+        }
+        return ciag(n - 1) * ciag(n - 2);
     }
+
+  /*  public static int silna(int n){
+        System.out.println(n);
+        if(n<2){
+        return n * silnia(n -1);
+    }
+*/
+    //private static int silnia(int n) {
+       // return 1;
+   // }
+
+    public static int ciag2(int n){
+if (n==-1){
+    return -1;
 }
+return ~ciag2(n-1) * n-3;
+
+}
+
+    public static void main( String[] args ){
+        int silnia = 5;
+        int wynik = 1;
+        for (int i=1; i<=silnia; i++){
+            wynik = wynik *i;
+        }
+        System.out.println("Silnia: " + wynik);
+    }
+
+}
+
+
---
+++ src/main/java/com/sda/algorytmy/Zad3.java
@@ -0,0 +1,5 @@
+package com.sda.algorytmy;
+
+public class Zad3 {
+
+}
