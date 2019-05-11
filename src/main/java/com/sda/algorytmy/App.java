5bc53b4e2ba95e4ca53128945628974d30f60929
Michal Stusik <x5htpx@gmail.com>
2019-05-11 13:31:20 +0100

    inicializacja repozytorium

---
+++ src/main/java/com/sda/algorytmy/App.java
@@ -0,0 +1,13 @@
+package com.sda.algorytmy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
